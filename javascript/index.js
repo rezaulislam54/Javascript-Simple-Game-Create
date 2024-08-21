f087777e38bdc2382fcea3ba64dc6d8cc4c19c85
rezaulislam54 <rezaulislam5490@gmail.com>
2024-08-21 23:58:42 +0600

    added a javascript function life and score update

--- javascript/index.js
+++ javascript/index.js
@@ -1,14 +1,45 @@
+//  Add class list
 function addClassById (elementId){
     const fristSection = document.getElementById(elementId);
     fristSection.classList.add('hidden')
 }
 
 
+// remove class list
 function removeClassById (elementId){
     const scondSection = document.getElementById(elementId);
      scondSection.classList.remove('hidden')
 }
 
+
+// score and life inner text get
+function geteTextElementValuById(elementId){
+    const elementValu = document.getElementById(elementId);
+    const elementValuText = elementValu.innerText;
+    const valu = parseInt(elementValuText);
+    return valu;
+}
+
+// score and life value set
+function setTextElementValuById(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
+
+// key bacground color set
+function setBacground (elementId){
+    const bgId = document.getElementById(elementId);
+    bgId.classList.add('bg-orange-400');
+}
+
+// key bacground color remove
+function removeBacground (elementId){
+    const bgId = document.getElementById(elementId);
+    bgId.classList.remove('bg-orange-400');
+}
+
+
+// gete random number
 function geteRandomNumber(){
     const simpleString = 'abcdefghijklmnopqrstuvwxyz';
     const alphabets = simpleString.split('');
--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,40 @@
-function continuePlay(){
+
+// keyboard pressed pleyar
+
+function keyBoardKeyUpEvent(event){
+    const playerPressed = event.key;
+    const alphabetDisplay = document.getElementById('display-alphabet');
+    const currentAlphabet = alphabetDisplay.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    
+    if(playerPressed === expectedAlphabet){
+        const currentScore = geteTextElementValuById('current-score');
+        const newScore = currentScore + 1;
+        setTextElementValuById('current-score', newScore);
+
+        removeBacground(playerPressed);
+        continueGame();
+    }
+    else{
+        const currentLife = geteTextElementValuById('current-life');
+        const newLife = currentLife - 1;
+        setTextElementValuById('current-life', newLife);
+    }   
+}
+
+document.addEventListener('keyup', keyBoardKeyUpEvent);
+
+
+function continueGame(){
     const alphabet = geteRandomNumber();
     const alphabetDisplay = document.getElementById('display-alphabet');
     alphabetDisplay.innerText = alphabet;
+    setBacground(alphabet);
 }
 
 
 function playGame(){
     addClassById('frist-home');
     removeClassById('scond-home-screen');
-    continuePlay();
+    continueGame();
 }
